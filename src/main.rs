5006c8d85270a345d6510affb9e4b98926bcb706
MatiF100 <mateuszfesz@wp.pl>
2024-01-18 00:01:59 +0100

    Initial tests

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-use actix_web::web::get;
-
-async fn greet(req: HttpRequest) -> impl Responder{
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(||{
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health_check", get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
-
-async fn health_check() -> impl Responder{
-    HttpResponse::Ok()
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address!");
+    run(listener)?.await
 }
