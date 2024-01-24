e7058bc9aa40d5cb009bbbafe6edf3f917e371f5
MatiF100 <mateuszfesz@wp.pl>
2024-01-24 01:24:39 +0100

    File tree refactoring

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,10 @@
+use actix_web::{HttpResponse, Responder, web};
+
+#[derive(serde::Deserialize)]
+pub struct FormData{
+    pub name: String,
+    pub email: String,
+}
+pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
